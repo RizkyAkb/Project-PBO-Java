3948e21d7aa130a8720c4e40f250a1ea2ff09e03
Envy Noxious <ahmadadli02@student.uns.ac.id>
2022-12-29 11:56:11 +0700

    Kerja

---
+++ ProjectUAS/src/varian/BahanVarian.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package varian;
+
+/**
+ *
+ * @author athrees
+ */
+public abstract class BahanVarian {
+    double keju;
+    double coklat;
+    double krim;
+    double selai;
+    double sosis;
+    double beef;
+    double bawang;
+    
+    public abstract double Varian1();
+}
---
+++ ProjectUAS/src/varian/RotiManis.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package varian;
+import bahan.*;
+
+/**
+ *
+ * @author athrees
+ */
+public class RotiManis extends BahanVarian{
+    
+    @Override
+    public double Varian1() {
+        this.keju
+    }
+}
