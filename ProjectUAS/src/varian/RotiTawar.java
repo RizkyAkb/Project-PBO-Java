b08e70383126cf3f58e4ceb6dfdb1bd31e10d637
melatinurmalasari <melati.nur.malasari8@student.uns.ac.id>
2022-12-29 13:49:50 +0700

    harga jual roti tawar

--- ProjectUAS/src/varian/RotiTawar.java
+++ ProjectUAS/src/varian/RotiTawar.java
@@ -14,15 +14,17 @@ public class RotiTawar extends BahanVarian implements Varian2{
     @Override
     public double Varian1() {
         this.coklat = 80 * (29000/500);
+        double hargaVarian1 = this.coklat;
         double hargaJual = (hargaVarian1 + rotitawar.hitungHargaPerPcs()) + ((hargaVarian1 + rotitawar.hitungHargaPerKemasan())*(50/100));
-        return this.coklat;
+        return hargaJual;
     }
 
     @Override
     public double varian2() {
         this.keju = 80 * (31000/250);
+        double hargaVarian2 = this.keju;
         double hargaJual = (hargaVarian2 + rotitawar.hitungHargaPerPcs()) + ((hargaVarian2 + rotitawar.hitungHargaPerKemasan())*(50/100));
-        return this.keju;
+        return hargaJual;
     }
     
 }
