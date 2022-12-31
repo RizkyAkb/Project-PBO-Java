ae2bbc15a9baf9c1dda45351bff7269e6680eec1
melatinurmalasari <melati.nur.malasari8@student.uns.ac.id>
2022-12-31 13:52:02 +0700

    update yg pizza

--- ProjectUAS/src/varian/Pizza.java
+++ ProjectUAS/src/varian/Pizza.java
@@ -8,17 +8,39 @@ import bahan.*;
  *
  * @author athrees
  */
-public class Pizza extends BahanVarian{
-    HitungBahanPizza Pizza = new HitungBahanPizza();
+public class Pizza extends HitungBahanPizza implements Varian1{
+//    HitungBahanPizza Pizza = new HitungBahanPizza();
+    double keju;
+    double sosis;
+    double beef;
+    double bawang;
+    double jumlahPesan;
 
     @Override
-    public double Varian1() {
-        this.keju = 30 * (31000/250);
-        this.sosis = 50 * (80000/1000);
-        this.beef = 50 * (90000/1000);
-        this.bawang = 30 * (40000/500);
-        double hargaVarian1 = this.keju + this.sosis + this. beef + this.bawang;
-        double hargaJual = (hargaVarian1 + Pizza.hitungHargaPerPcs()) + ((hargaVarian1 + Pizza.hitungHargaPerKemasan())*(50/100));
-        return hargaJual;
+    public double varian1(double jumlahPesan) {
+        this.jumlahPesan = jumlahPesan;
+        this.hitungBeratBahan(jumlahPesan);
+        this.keju = Math.ceil(30 * jumlahPesan / 250);
+        this.sosis = Math.ceil(50 * jumlahPesan / 1000);
+        this.beef = Math.ceil(50 * jumlahPesan / 1000);
+        this.sosis = Math.ceil(30 * jumlahPesan / 500);
+        double hrgKeju = this.keju * 31000;
+        double hrgSosis = this.sosis * 80000;
+        double hrgBeef = this.beef * 80000;
+        double hrgBawang = this.bawang * 40000;
+        double hargaVarian1 = hrgKeju + hrgSosis + hrgBeef + hrgBawang ;
+        double totalKeuntungan = (hargaVarian1 + this.totalHargaBeli()) + ((hargaVarian1 + this.totalHargaBeli())*(50.0/100.0));
+        double hargaSatuan = Math.ceil(totalKeuntungan/jumlahPesan);
+        return hargaSatuan;
+    }
+    
+    
+    public void tampilVarian1(){
+        System.out.println("Varian         Berat diperlukan     Jumlah Kemasan");
+    }
+
+    private double totalHargaBeli(){
+        return 0;
+        
     }
 }
